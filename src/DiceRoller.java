176a0fc258c1bad763d83b380d50693811b90537
James Bryant II <jcbryant718@gmail.com>
2017-02-06 19:39:23 -0500

    final message

--- src/DiceRoller.java
+++ src/DiceRoller.java
@@ -1,57 +1,33 @@
 import java.util.Scanner;
 public class DiceRoller {
     public static void main(String[] args) {
-        //Greet User!!!
-        System.out.println("Hello and Welcome to Chris' new game called Rolling the Dice! ");
-
-        //1. Prompt user to enter the number of sides of the dice
-        System.out.println("How many sides would you like your dice to have? ");
-
-        //2. Get user input
         Scanner scan = new Scanner(System.in);
 
-        int sides = scan.nextInt();
-        //3. Prompt user to roll the dice
-
-        System.out.println("To roll the dice please press any number: ");
-        //4. Get user input
-
-        scan.nextInt();
+        int sides;
+        String answer = "";
 
 
-        //5. call the methods
-        int result1 = diceRoll(sides);
-        int result2 = diceRoll(sides);
+        //Greet User!!!
+        System.out.println("Hello and Welcome to James' new game called Rolling the Dice! ");
+        System.out.println("");
+        System.out.println("");
 
-        //6. Display results
-        System.out.println("Your results are: " + result1 + " and " + result2);
+        do {
+            System.out.println("Enter the number of sides on your die: ");
+            sides = scan.nextInt();
 
+            // call the methods
 
+            //6. Display results
+            System.out.println("Your results are: " + RollDice(sides) + " and " + RollDice(sides));
+
+            System.out.println("Do You Want to Continue? y/n");
+            answer = scan.next();
+            } while (answer.equals("y"));
     }
 
-    private static int diceRoll(int sides) {
-        return 0;
-    }
+        public static int RollDice ( int sides){
+            return (int) (Math.random() * sides) + 1;
+            } //end of method
 
-    public class RollTheDice {
-
-    /*  This program simulates rolling a pair of dice.
-        The number that comes up on each die is output,
-        followed by the total of the two dice.
-    */
-
-
-
-
-    }  // end main()
-
-}  // end class
-
-
-
-//3. prompt user to roll the dice
-//4. get user input
-//5. perform the dice roll
-//6. display results
-//7. prompt user to continue
-//8. get user choice
+    } // end main
